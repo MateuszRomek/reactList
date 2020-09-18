9eb868ea3d1dc4f69dedc9fd21cc1dfcfaebcef6
Mateusz <mateusz.romek@outlook.com>
2020-09-18 16:25:20 +0200

    Test with new todos route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import GlobalStyle from './styles/GlobalStyle';
 import SignUpLogInTemplate from './templates/SignUpLogIn/SignLoginTemplate';
+import RootViewTemplate from './templates/RootView/RootViewTemplate';
 const App: React.FC = () => {
 	return (
 		<>
 			<GlobalStyle />
-			<SignUpLogInTemplate />
+			<Switch>
+				<Route path="/todos" component={RootViewTemplate} />
+				<Route path="/" component={SignUpLogInTemplate} />
+			</Switch>
 		</>
 	);
 };
