7123221e16d745b5ebbc419cc17d81b4bb20e47b
Mateusz <mateusz.romek@outlook.com>
2020-09-18 16:38:40 +0200

    Created login function in auth controller. It check if user enter correct email and password and then response with user data and JWT Token

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,11 +1,11 @@
 const User = require('../models/user');
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
-const saltRounds = 12;
+const SALTROUNDS = 12;
+const jwt = require('jsonwebtoken');
 
 exports.createUser = (req, res, next) => {
 	//TODO connect mongodb to this and create user. Then res 200 status
-	console.log('Request incoming');
 	const { name, email, password } = req.body;
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
@@ -23,7 +23,7 @@ exports.createUser = (req, res, next) => {
 	}
 
 	bcrypt
-		.genSalt(saltRounds)
+		.genSalt(SALTROUNDS)
 		.then((salt) => {
 			return bcrypt.hash(password, salt);
 		})
@@ -53,4 +53,54 @@ exports.createUser = (req, res, next) => {
 				next(error);
 			}
 		});
+};
+
+exports.logInUser = async (req, res, next) => {
+	const { email, password } = req.body;
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation failed');
+		error.statusCode = 422;
+		error.errors = errors.array().map((error) => {
+			const { param, msg } = error;
+			return {
+				[param]: {
+					errorText: msg,
+				},
+			};
+		});
+		throw error;
+	}
+	const user = await User.findOne({ email });
+	if (!user) {
+		const error = new Error('There is no account with this e-mail');
+		error.statusCode = 404;
+		next(error);
+	}
+	const match = await bcrypt.compare(password, user.password);
+	if (!match) {
+		const error = new Error('Incorrect password');
+		error.statusCode = 401;
+		next(error);
+	}
+	jwt.sign(
+		{
+			uId: user._id.toString(),
+		},
+		process.env.SECRET,
+		{ expiresIn: 1800 },
+		function (err, token) {
+			if (err) {
+				console.log(err);
+				const error = new Error('Internal error has occured');
+				next(error);
+			} else {
+				res.status(200).json({
+					token,
+					name: user.name,
+					uid: user._id,
+				});
+			}
+		}
+	);
 };
