37c18ab8fd27ba3435d03c54ece49875a9ea2678
Mateusz <mateusz.romek@outlook.com>
2020-09-21 22:05:04 +0200

    Changes to theme object for styled components

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -10,6 +10,10 @@ const themeData: theme = {
 		deepSea: 'rgb(43, 122, 120)',
 		lightBlue: 'rgb(222, 242, 241)',
 		darkText: 'rgb(23, 37, 42)',
+		borderGrayColor: 'rgb(229, 229, 229)',
+		lightGray: 'rgb(244, 244, 244)',
+		selectedListLight: 'rgb(237,237,237)',
+		hoverListLight: 'rgb(255,255,255)',
 	},
 };
 
