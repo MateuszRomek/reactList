f7e6bd17dddc49e553663b42fd45fc0bf7709609
Mateusz <mateusz.romek@outlook.com>
2020-09-22 20:57:18 +0200

    RootView changed to flex from grid

--- src/views/RootView/RootView.tsx
+++ src/views/RootView/RootView.tsx
@@ -2,23 +2,17 @@ import React from 'react';
 import styled from 'styled-components';
 import UserNavigation from '../../components/UserNavigation/UserNavigation';
 const RootGrid = styled.div`
-	display: grid;
-	grid-template-columns: 260px 1fr 400px;
+	display: flex;
+	flex: 1 1 0px;
+	will-change: width;
 	height: 100vh;
 	overflow: hidden;
-`;
-
-const LeftContainer = styled.div`
-	border-right: 1px solid ${({ theme }) => theme.colors.borderGrayColor};
-	background-color: ${({ theme }) => theme.colors.lightGray};
 `;
 
 const RootViewTemplate: React.FC = () => {
 	return (
 		<RootGrid>
-			<LeftContainer>
-				<UserNavigation />
-			</LeftContainer>
+			<UserNavigation />
 		</RootGrid>
 	);
 };
