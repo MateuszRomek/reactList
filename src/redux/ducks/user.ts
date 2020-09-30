afd52d4826af84b8640d78ee353f714effa07348
Mateusz <mateusz.romek@outlook.com>
2020-09-30 17:13:28 +0200

    Created function to get user data on page refresh.

--- src/redux/ducks/user.ts
+++ src/redux/ducks/user.ts
@@ -1,7 +1,11 @@
+import { Dispatch } from 'react';
 import {
 	IinitialState,
 	UserActionTypes,
 	SET_USER_DATA,
+	GET_USER_START,
+	GET_USER_FINISH,
+	GET_USER_FAILED,
 } from '../types/userTypes';
 
 const initialState: IinitialState = {
@@ -41,3 +45,60 @@ export const setUserData = (
 	name,
 	email,
 });
+
+type getUserDataStep = 'start' | 'finish' | 'fail';
+const getUserData = (step: getUserDataStep): UserActionTypes => {
+	switch (step) {
+		case 'start':
+			return {
+				type: GET_USER_START,
+			};
+		case 'finish':
+			return {
+				type: GET_USER_FINISH,
+			};
+
+		case 'fail': {
+			return {
+				type: GET_USER_FAILED,
+			};
+		}
+
+		default: {
+			return {
+				type: GET_USER_FAILED,
+			};
+		}
+	}
+};
+
+export const getUserOnRefresh = () => {
+	return async (dispatch: Dispatch<UserActionTypes>) => {
+		const t = localStorage.getItem('token');
+		dispatch(getUserData('start'));
+		try {
+			const response = await fetch('http://localhost:8080/userInf', {
+				method: 'GET',
+				headers: {
+					Authorization: `Bearer  ${t}`,
+					'Content-Type': 'application/json',
+				},
+			});
+			const data: {
+				userId: string;
+				name: string;
+				email: string;
+				status?: number;
+				message: string;
+			} = await response.json();
+			if (data.status !== 200) {
+				throw data;
+			}
+			const { email, name, userId } = data;
+			dispatch(setUserData(userId, name, email));
+			dispatch(getUserData('finish'));
+		} catch (err) {
+			dispatch(getUserData('fail'));
+		}
+	};
+};
--- src/redux/types/userTypes.ts
+++ src/redux/types/userTypes.ts
@@ -1,4 +1,8 @@
 export const SET_USER_DATA = 'SET_USER_DATA';
+export const GET_USER_START = 'GET_USER_START';
+export const GET_USER_FINISH = 'GET_USER_FINISH';
+export const GET_USER_FAILED = 'GET_USER_FAILED';
+
 export interface IinitialState {
 	userId: string;
 	name: string;
@@ -12,7 +16,21 @@ export interface SetUserData {
 	email: string;
 }
 
-export type UserActionTypes = SetUserData;
+export interface GetUserDataStart {
+	type: typeof GET_USER_START;
+}
+export interface GetUserDataFinished {
+	type: typeof GET_USER_FINISH;
+}
+
+export interface GetUserDataFailed {
+	type: typeof GET_USER_FAILED;
+}
+export type UserActionTypes =
+	| SetUserData
+	| GetUserDataStart
+	| GetUserDataFinished
+	| GetUserDataFailed;
 
 export interface IuserReducer {
 	user: IinitialState;
