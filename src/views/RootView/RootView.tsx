9ae0ab24fb3f1c29be3286629f8a17301856460a
Mateusz <mateusz.romek@outlook.com>
2020-09-30 17:14:29 +0200

    Removed getUserInfo fn. Added useEffect with redux dispatch action in RootView component

--- src/views/RootView/RootView.tsx
+++ src/views/RootView/RootView.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 import UserNavigation from '../../components/UserNavigation/UserNavigation';
-import TasksList from '../../components/TasksList/TasksList';
+import TasksList from '../../components/TasksContainer/TasksContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { IlistsReducer } from '../../redux/types/listsTypes';
+import { IuserReducer } from '../../redux/types/userTypes';
+import { getUserOnRefresh } from '../../redux/ducks/user';
 const RootGrid = styled.div`
 	display: flex;
 	flex: 1 1 0px;
@@ -11,10 +15,26 @@ const RootGrid = styled.div`
 `;
 
 const RootViewTemplate: React.FC = () => {
+	const currentList = useSelector(
+		(state: IlistsReducer) => state.lists.currentList
+	);
+
+	const { email, name, userId } = useSelector(
+		(state: IuserReducer) => state.user
+	);
+	const dispatch = useDispatch();
+	const stableDispatch = useCallback(dispatch, []);
+	useEffect(() => {
+		if (email === '' && name === '' && userId === '') {
+			stableDispatch(getUserOnRefresh());
+		} else {
+			return;
+		}
+	}, [stableDispatch, email, name, userId]);
 	return (
 		<RootGrid>
 			<UserNavigation />
-			<TasksList />
+			{currentList._id === '' && currentList.name === '' ? null : <TasksList />}
 		</RootGrid>
 	);
 };
