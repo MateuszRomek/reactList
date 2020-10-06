6e1850aa7a4b96a89e5a0247fdae2a655d24bc86
Mateusz <mateusz.romek@outlook.com>
2020-10-06 19:07:46 +0200

    Created emoji select modal. User can now select own emoji for his list\

---
+++ src/components/TasksContainer/SelectEmoji/SelectEmoji.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styled from 'styled-components';
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
+import { useDispatch } from 'react-redux';
+import {
+	postUpdateListData,
+	updateListEmoji,
+} from '../../../redux/ducks/lists';
+
+interface Props {
+	isEmojiActive: boolean;
+	top: string;
+	setEmojiActive: React.Dispatch<React.SetStateAction<boolean>>;
+	listId: string;
+}
+interface StyledProps {
+	isActive: boolean;
+	top?: string;
+}
+
+const EmojiContainer = styled.div<StyledProps>`
+	position: absolute;
+	top: ${({ top }) => top};
+	left: 20px;
+	opacity: ${({ isActive }) => (isActive ? 1 : 0)};
+	pointer-events: ${({ isActive }) => (isActive ? 'all' : 'none')};
+	z-index: 20;
+`;
+const Backdrop = styled.div<StyledProps>`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: transparent;
+	opacity: ${({ isActive }) => (isActive ? 1 : 0)};
+	pointer-events: ${({ isActive }) => (isActive ? 'all' : 'none')};
+	z-index: 10;
+`;
+const SelectEmoji: React.FC<Props> = ({
+	isEmojiActive,
+	top,
+	setEmojiActive,
+	listId,
+}) => {
+	const dispatch = useDispatch();
+	return (
+		<Backdrop
+			data-bd="backdrop"
+			onClick={(e) => {
+				const data = (e.target as Element).getAttribute('data-bd');
+				if (data) {
+					setEmojiActive(false);
+				} else {
+					return;
+				}
+			}}
+			isActive={isEmojiActive}
+		>
+			<EmojiContainer isActive={isEmojiActive} top={top}>
+				<Picker
+					native
+					onSelect={(emoji) => {
+						const em = emoji.native as string;
+						dispatch(updateListEmoji(em));
+						const t = localStorage.getItem('token');
+						dispatch(postUpdateListData(t, listId, em, 'emoji'));
+						setEmojiActive(false);
+					}}
+					showPreview={false}
+					showSkinTones={false}
+					title="Pick your emoji…"
+				/>
+			</EmojiContainer>
+		</Backdrop>
+	);
+};
+
+export default SelectEmoji;
