358b606777dc8710fabbf886e5a807c7c9973d2b
Mateusz <mateusz.romek@outlook.com>
2020-10-07 21:08:04 +0200

    Created todo reducer with todo types. Created basic functions for fetching user todos and for creation of the new todo

---
+++ src/redux/ducks/todo.ts
@@ -0,0 +1,147 @@
+import { Dispatch } from 'react';
+import {
+	ITodoInitialState,
+	TodoActionTypes,
+	FETCH_TODOS_START,
+	FETCH_TODOS_FINISHED,
+	FETCH_TODOS_FAILED,
+	CREATE_NEW_TODO,
+	CreatedTodoResponse,
+	POST_TODO_START,
+	POST_TODO_FINISHED,
+	POST_TODO_FAILED,
+	Todo,
+	ServerTodoLogActions,
+	SET_TODOS,
+	FetchTodosResponse,
+} from './../types/todoTypes';
+import { addTodo } from './lists';
+
+const initialState: ITodoInitialState = {
+	todos: [],
+	currentTodo: {
+		title: '',
+		_id: '',
+		isChecked: false,
+		description: '',
+	},
+};
+
+const reducer = (
+	state = initialState,
+	action: TodoActionTypes
+): ITodoInitialState => {
+	switch (action.type) {
+		case CREATE_NEW_TODO: {
+			const { title, _id } = action;
+			const todoObj: Todo = {
+				description: '',
+				isChecked: false,
+				title,
+				_id,
+			};
+
+			return {
+				...state,
+				todos: [...state.todos, todoObj],
+			};
+		}
+
+		case SET_TODOS: {
+			const { todos } = action;
+			return {
+				...state,
+				todos: todos,
+			};
+		}
+		default: {
+			return state;
+		}
+	}
+};
+
+export default reducer;
+
+const postTodoStart = (): ServerTodoLogActions => ({
+	type: POST_TODO_START,
+});
+const postTodoFinished = (): ServerTodoLogActions => ({
+	type: POST_TODO_FINISHED,
+});
+const postTodoFailed = (): ServerTodoLogActions => ({
+	type: POST_TODO_FAILED,
+});
+
+const fetchTodosStart = (): ServerTodoLogActions => ({
+	type: FETCH_TODOS_START,
+});
+const fetchTodosFinished = (): ServerTodoLogActions => ({
+	type: FETCH_TODOS_FINISHED,
+});
+const fetchTodosFailed = (): ServerTodoLogActions => ({
+	type: FETCH_TODOS_FAILED,
+});
+const createNewTodo = (_id: string, title: string): TodoActionTypes => ({
+	type: CREATE_NEW_TODO,
+	_id,
+	title,
+});
+const setTodos = (todos: Todo[]): TodoActionTypes => ({
+	type: SET_TODOS,
+	todos,
+});
+export const postNewTodo = (
+	token: string | null,
+	title: string,
+	listId: string
+) => {
+	return (dispatch: Dispatch<ServerTodoLogActions | TodoActionTypes>) => {
+		dispatch(postTodoStart());
+
+		fetch('http://localhost:8080/todo', {
+			method: 'POST',
+			headers: {
+				Authorization: `Bearer  ${token}`,
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				title,
+				listId,
+			}),
+		})
+			.then((response) => response.json())
+			.then((result: CreatedTodoResponse) => {
+				if (result.status !== 201) throw result;
+				const {
+					todo: { _id, title },
+				} = result;
+				dispatch(postTodoFinished());
+				dispatch(createNewTodo(_id, title));
+				dispatch(addTodo(_id));
+			})
+			.catch((e) => {
+				dispatch(postTodoFailed());
+			});
+	};
+};
+
+export const fetchUserTodos = (token: string | null) => {
+	return (dispatch: Dispatch<ServerTodoLogActions | TodoActionTypes>) => {
+		dispatch(fetchTodosStart());
+		fetch('http://localhost:8080/todo', {
+			method: 'GET',
+			headers: {
+				Authorization: `Bearer  ${token}`,
+				'Content-Type': 'application/json',
+			},
+		})
+			.then((respone) => respone.json())
+			.then((result: FetchTodosResponse) => {
+				if (result.status !== 200) throw result;
+				dispatch(fetchTodosFinished());
+				console.log(result);
+				dispatch(setTodos(result.todos));
+			})
+			.catch((e) => dispatch(fetchTodosFailed()));
+	};
+};
---
+++ src/redux/types/todoTypes.ts
@@ -0,0 +1,77 @@
+import { IAddNewTodo } from './listsTypes';
+
+export const SET_CURRENT_TODO = 'SET_CURRENT_TODO';
+export const FETCH_USER_TODOS = 'FETCH_USER_TODOS';
+export const FETCH_TODOS_FINISHED = 'FETCH_TODOS_FINISHED';
+export const FETCH_TODOS_START = 'FETCH_TODOS_START';
+export const FETCH_TODOS_FAILED = 'FETCH_TODOS_FAILED';
+export const CREATE_NEW_TODO = 'CREATE_NEW_TODO';
+export const POST_TODO_START = 'POST_TODO_START';
+export const POST_TODO_FINISHED = 'POST_TODO_FINISHED';
+export const POST_TODO_FAILED = 'POST_TODO_FAILED';
+export const SET_TODOS = 'SET_TODOS';
+export interface Todo {
+	title: string;
+	isChecked: boolean;
+	description: string;
+	_id: string;
+}
+
+export interface ITodoInitialState {
+	todos: Todo[] | [];
+	currentTodo: Todo;
+}
+export interface TodoSelector {
+	todo: ITodoInitialState;
+}
+
+interface ISetCurrentTodo {
+	type: typeof SET_CURRENT_TODO;
+	todoId: string;
+}
+interface IFetchTodosInfo {
+	type:
+		| typeof FETCH_TODOS_START
+		| typeof FETCH_TODOS_FINISHED
+		| typeof FETCH_TODOS_FAILED;
+}
+
+interface IPostTodoInfo {
+	type:
+		| typeof POST_TODO_START
+		| typeof POST_TODO_FINISHED
+		| typeof POST_TODO_FAILED;
+}
+
+interface ICreateNewTodo {
+	type: typeof CREATE_NEW_TODO;
+	title: string;
+	_id: string;
+}
+
+interface BasicResponseData {
+	status: number;
+	message: string;
+}
+export interface CreatedTodoResponse extends BasicResponseData {
+	todo: {
+		title: string;
+		_id: string;
+	};
+}
+export interface FetchTodosResponse extends BasicResponseData {
+	todos: Todo[];
+}
+
+export interface ISetTodos {
+	type: typeof SET_TODOS;
+	todos: Todo[];
+}
+
+export type TodoActionTypes =
+	| ISetTodos
+	| ISetCurrentTodo
+	| ICreateNewTodo
+	| IAddNewTodo;
+
+export type ServerTodoLogActions = IPostTodoInfo | IFetchTodosInfo;
