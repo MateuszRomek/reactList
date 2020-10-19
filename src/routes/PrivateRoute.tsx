fd0ea303e5daed178ad133c42ea93dc40aa3f08f
Mateusz <mateusz.romek@outlook.com>
2020-10-19 19:35:14 +0200

    Changes in private route. Added use selector for auth and isLoading property from user state

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { getUserOnRefresh } from '../redux/ducks/user';
 import { IuserReducer } from '../redux/types/userTypes';
 
 const PrivateRoute = ({ component: Component, ...rest }: any) => {
-	const selector = useSelector(({ user }: IuserReducer) => ({
-		name: user.name,
-		userId: user.userId,
-		email: user.email,
-	}));
-	const v =
-		localStorage.getItem('token') &&
-		selector.name &&
-		selector.userId &&
-		selector.email
-			? true
-			: false;
+	const isAuth = useSelector((state: IuserReducer) => state.user.isAuth);
+	const isLoading = useSelector((state: IuserReducer) => state.user.isLoading);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(getUserOnRefresh());
+	}, [dispatch]);
+
+	if (isLoading) {
+		return <div></div>;
+	}
 
 	return (
 		<Route
 			{...rest}
-			render={(props) => (v ? <Component {...props} /> : <Redirect to="/" />)}
+			render={(props) =>
+				isAuth ? <Component {...props} /> : <Redirect to="/" />
+			}
 		/>
 	);
 };
